613135c7bb91617bf6615d88da488810d6c3f327
alamin-asr <156467396+alamin-asr@users.noreply.github.com>
2025-06-04 17:19:44 +0600

    first commit

---
+++ src/main/java/com/alamin/shipping_update/ShippingUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.alamin.shipping_update;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alamin/shipping_update/ShippingUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamin.shipping_update;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
